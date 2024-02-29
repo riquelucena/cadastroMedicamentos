b522ec92bb061e65e25b96a74765bfbec3f88727
luizhenriquelucena <riquelucena@gmail.com>
2024-02-29 17:30:37 -0300

    feat(6): Criação da lógica para listagem dos medicamentos cadastrados.
    Criação da lógica para listagem dos medicamentos cadastrados no localStorage.
    Início da implementação para edição e exclusão dos medicamentos cadastrados.

---
+++ js/listagem.js
@@ -0,0 +1,44 @@
+let medicamento = localStorage.getItem("medicamentos");
+    let medicamentoObj = JSON.parse(medicamento);
+    const medicamentos = medicamentoObj;
+
+function criarListaDeMedicamentos() {
+    if (medicamentos.length === 0) {
+        document.getElementById("tabelaMedicamentos").innerHTML = "Nenhum medicamento cadastrado.";
+        return;
+    }
+    
+    let tabelaHTML = "<table border='1'><tr><th>Nome</th><th>Preço</th><th>Quantidade</th><th>Ações</th></tr>";
+    
+    for (let i = 0; i < medicamentos.length; i++) {
+        tabelaHTML += `<tr>
+                           <td>${medicamentos[i].nome}</td>
+                           <td>${medicamentos[i].preco}</td>
+                           <td>${medicamentos[i].quantidade}</td>
+                           <td>
+                              <button type="button" onclick='editarMedicamento(${i})'>Editar</button>
+                              <button type="button" onclick='excluirMedicamento(${i})'>Excluir</button>
+                           </td>
+                       </tr>`;
+    }
+    
+    tabelaHTML += "</table>";
+    
+    document.getElementById("tabelaMedicamentos").innerHTML = tabelaHTML;
+}
+    
+window.onload = function() {
+    criarListaDeMedicamentos();
+};
+
+function editarMedicamento(dado) {
+    const medicamentoEd = medicamentos[dado];
+
+    sessionStorage.setItem("medicamentos", JSON.stringify(medicamentoEd));
+
+    window.location.href = "editar.html";
+}
+
+function excluirMedicamento() {
+    console.log('Medicamento Excluído')
+}
